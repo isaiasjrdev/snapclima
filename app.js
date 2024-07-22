95556e8ea213a7e5b3016d8a294a7fd9656bdeff
Isaias Junior <isaiscastro56@gmail.com>
2024-07-22 15:34:59 -0300

    projeto final

--- app.js
+++ app.js
@@ -1,56 +1,148 @@
+// //Interação
+// const citySearchInput = document.getElementById('city-search-input');
+// const citySearchButton = document.getElementById('city-search-button');
+
+// //Exibição
+// const currentDate = document.getElementById("current-date");
+// const cityName = document.getElementById("city-name");
+// const weatherIcon = document.getElementById("weather-icon");
+// const weatherDescription = document.getElementById("weather-description");
+// const currentTemperature = document.getElementById("current-temperature");
+// const windSpeed = document.getAnimations("wind-speed");
+// const feelsLikeTemperature = document.getElementById("feels-like-temperature");
+// const currentHumidity = document.getElementById("current-humidity");
+// const sunriseTime = document.getElementById("sunrise-time");
+// const sunsetTime = document.getElementById("sunset-time");
+
+// const api_key = "4bd089e1f0a9c4cb1edb6a82650dd84e"
+
+// citySearchButton.addEventListener("click", () => {
+//     let cityName = citySearchInput.value
+//     getCityWeather(cityName)
+// })
+
+// //https://api.openweathermap.org/data/3.0/onecall/overview?lat={lat}&lon={lon}&appid={API key}
+
+// navigator.geolocation.getCurrentPosition(
+//     (position) => {
+//         let lat = position.coords.latitude
+//         let lon = position.coords.longitude
+
+//         getCurrentLocationWeather(lat, lon)
+//     },
+//     (err) => {
+//         if (err.code === 1) {
+//             alert("Geolocalização negada pelo usuário, busque manualmente por uma cidade através da barra de pesquisa.")
+//         } else {
+//             console.log(err)
+//         }
+//     }
+// )
+
+// function getCurrentLocationWeather(lat, lon) {
+//     fetch(`https://https://api.openweathermap.org/data/3.0/onecall/overview?lat=${lat}&lon=${lon}&appid=${api_key}`) 
+//         .then((response) => response.json())
+//         .then((data) => displayWeather(data))
+// }
+
+// function getCityWeather(cityName) {
+
+//     weatherIcon.src=`./assets/loading-icon.svg`
+
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`) 
+//         .then((response) => response.json())
+//         .then((data) => displayWeather(data))
+// }
+
+// function displayWeather(data) {
+//     let {
+//         dt,
+//         name,
+//         weather: [{ icon, description }],
+//         main: { temp, feels_like, humidity },
+//         wind: { speed },
+//         sys: { sunrise, sunset },
+//         } = data
+
+//         currentDate.textContent = formatDate(dt);
+//         cityName.textContent = name;
+//         weatherIcon.src=`./assets/${icon}.svg`
+//         weatherDescription.textContent = description;
+//         currentTemperature.textContent = `${Math.round(temp)}ºC`;
+//         windSpeed.textContent = `${Math.round(speed * 3.6)}km/h`;
+//         feelsLikeTemperature.textContent = `${Math.round(feels_like)}ºC`;;
+//         currentHumidity.textContent = `${humidity}%`;
+//         sunriseTime.textContent = formatTime(sunrise);
+//         sunsetTime.textContent = formatTime(sunset);
+// }
+
+// function formatDate(epochTime) {
+//     let date = new Date(epochTime * 1000)
+//     let formattedDate = date.toLocaleDateString('pt-BR', { month: "long", day: 'numeric' })
+//     return `Hoje, ${formattedDate}`
+// }
+
+// function formatTime(epochTime) {
+//     let date = new Date(epochTime * 1000)
+//     let hours = date.getHours()
+//     let minutes = date.getMinutes()
+//     return `${hours}:${minutes}`
+// }
+
 //Interação
 const citySearchInput = document.getElementById('city-search-input');
 const citySearchButton = document.getElementById('city-search-button');
 
-//Exibição
-const currentDate = document.getElementById("current-date");
-const cityName = document.getElementById("city-name");
-const weatherIcon = document.getElementById("weather-icon");
-const weatherDescription = document.getElementById("weather-description");
-const currentTemperature = document.getElementById("current-temperature");
-const windSpeed = document.getAnimations("wind-speed");
-const feelsLikeTemperature = document.getElementById("feels-like-temperature");
-const currentHumidity = document.getElementById("current-humidity");
-const sunriseTime = document.getElementById("sunris-time");
-const sunsetTime = document.getElementById("sunset-time");
+//exibição
+const currentDate = document.getElementById('current-date');
+const cityName = document.getElementById('city-name');
+const weatherIcon = document.getElementById('weather-icon');
+const weatherDescription = document.getElementById('weather-description');
+const currentTemperature = document.getElementById('current-temperature');
+const windSpeed = document.getElementById('wind-speed');
+const feelsLIkeTemperature = document.getElementById('feels-like-temperature');
+const currentHumidity = document.getElementById('current-humidity');
+const sunriseTime = document.getElementById('sunrise-time');
+const sunsetTime = document.getElementById('sunset-time');
 
-const api_key = "7ada4e692feae11a59a0fc50219cc27a"
+const api_key = "0f1f609cb8d4a0df013d968a4f510e1a";
 
-citySearchButton.addEventListener("click", () => {
+citySearchButton.addEventListener('click', () => {
     let cityName = citySearchInput.value
     getCityWeather(cityName)
 })
 
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 navigator.geolocation.getCurrentPosition(
     (position) => {
-        let lat = position.coords.latitute
+        let lat = position.coords.latitude
         let lon = position.coords.longitude
 
-        getCurrentLocationWeather(lat, lon)
+        getCurrentLocationWeather(lat, lon )
     },
     (err) => {
         if (err.code === 1) {
-            alert("Geolocalização negada pelo usuário, busque manualmente por uma cidade através da barra de pesquisa.")
-        } else {
+            alert("Geolocalizçaõ negada pelo usuario")
+        }else{
             console.log(err)
         }
     }
 )
 
-function getCurrentLocationWeather(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${api_key}`) 
-    .then((response) => response.json())
-    .then((data) => displayWeather(data))
+function getCurrentLocationWeather(lat , lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${api_key}`)
+        .then((response) => response.json())
+        .then((data) => displayWeather(data))
 }
 
 function getCityWeather(cityName) {
 
-    weatherIcon.src=`./assets/loading-icon.svg`
+    weatherIcon.src = `./assets/loading-icon.svg `
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`) 
-    .then((response) => response.json())
-    .then((data) => displayWeather(data))
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`)
+        .then((response) => response.json())
+        .then((data) => displayWeather(data))
 }
 
 function displayWeather(data) {
@@ -58,18 +150,18 @@ function displayWeather(data) {
         dt,
         name,
         weather: [{ icon, description }],
-        main: { temp, feels_like, humidity },
+        main: { temp , feels_like, humidity },
         wind: { speed },
         sys: { sunrise, sunset },
         } = data
-
+        
         currentDate.textContent = formatDate(dt);
         cityName.textContent = name;
-        weatherIcon.src=`./assets/${icon}.svg`
+        weatherIcon.src = `./assets/${icon}.svg `
         weatherDescription.textContent = description;
         currentTemperature.textContent = `${Math.round(temp)}ºC`;
-        windSpeed.textContent = `${Math.round(speed * 3.6)}km/h`;
-        feelsLikeTemperature.textContent = `${Math.round(feels_like)}ºC`;;
+        windSpeed.textContent = `${Math.round(speed * 3.6)}Km/H`;
+        feelsLIkeTemperature.textContent = `${Math.round(feels_like)}ºC`;
         currentHumidity.textContent = `${humidity}%`;
         sunriseTime.textContent = formatTime(sunrise);
         sunsetTime.textContent = formatTime(sunset);
@@ -77,8 +169,9 @@ function displayWeather(data) {
 
 function formatDate(epochTime) {
     let date = new Date(epochTime * 1000)
-    let formattedDate = date.toLocaleDateString('pt-BR', { month: "long", day: 'numeric' })
+    let formattedDate = date.toLocaleDateString('pt-BR', {month: "long", day: 'numeric'})
     return `Hoje, ${formattedDate}`
+    
 }
 
 function formatTime(epochTime) {
