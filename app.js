f3e9e95a9c56586bf1f1302860f95c54c73f3cec
Isaias Junior <isaiscastro56@gmail.com>
2024-07-22 11:16:41 -0300

    correção de bug ao localizar cidade

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ citySearchButton.addEventListener("click", () => {
     getCityWeather(cityName)
 })
 
+
 navigator.geolocation.getCurrentPosition(
     (position) => {
         let lat = position.coords.latitute
         let lon = position.coords.longitude
 
         getCurrentLocationWeather(lat, lon)
-
     },
     (err) => {
         if (err.code === 1) {
@@ -46,7 +46,7 @@ function getCurrentLocationWeather(lat, lon) {
 
 function getCityWeather(cityName) {
 
-    weatherIcon.src=`/assets/loading-icon.svg`
+    weatherIcon.src=`./assets/loading-icon.svg`
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`) 
     .then((response) => response.json())
@@ -54,25 +54,25 @@ function getCityWeather(cityName) {
 }
 
 function displayWeather(data) {
-    let  {
+    let {
         dt,
         name,
         weather: [{ icon, description }],
         main: { temp, feels_like, humidity },
         wind: { speed },
         sys: { sunrise, sunset },
-    } = data
+        } = data
 
-    currentDate.textContent = formatDate(dt);
-    cityName.textContent = name;
-    weatherIcon.src=`/assets/${icon}.svg`
-    weatherDescription.textContent = description;
-    currentTemperature.textContent = `${Math.round(temp)}ºC`;
-    windSpeed.textContent = `${Math.round(speed * 3.6)}km/h`;
-    feelsLikeTemperature.textContent = `${Math.round(feels_like)}ºC`;;
-    currentHumidity.textContent = `${humidity}%`;
-    sunriseTime.textContent = formatTime(sunrise);
-    sunsetTime.textContent = formatTime(sunset);
+        currentDate.textContent = formatDate(dt);
+        cityName.textContent = name;
+        weatherIcon.src=`./assets/${icon}.svg`
+        weatherDescription.textContent = description;
+        currentTemperature.textContent = `${Math.round(temp)}ºC`;
+        windSpeed.textContent = `${Math.round(speed * 3.6)}km/h`;
+        feelsLikeTemperature.textContent = `${Math.round(feels_like)}ºC`;;
+        currentHumidity.textContent = `${humidity}%`;
+        sunriseTime.textContent = formatTime(sunrise);
+        sunsetTime.textContent = formatTime(sunset);
 }
 
 function formatDate(epochTime) {
